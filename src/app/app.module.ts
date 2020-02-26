7d1595967ca05d85fdd416dcefac7dbb9a73b127
BrunaSuelen <>
2020-02-26 19:15:01 -0300

    Adição do componente mention

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MentionModule } from 'angular-mentions';
 
 import { AppComponent } from './app.component';
+import { MentionComponent } from './mention/mention.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MentionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MentionModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
